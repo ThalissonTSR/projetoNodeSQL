90d7f0d040245dc66720cf42b79773823f4d8e50
ThalissonTSR <thalissontharl@gmail.com>
2025-05-19 16:38:17 -0300

    up

--- models/db.js
+++ models/db.js
@@ -1,16 +1,31 @@
 const Sequelize = require("sequelize");
 
+// Substitua pelos valores reais ou insira as variáveis diretamente
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  'railway',        // Nome do banco de dados
+  'root',     // Usuário do banco de dados
+  'FgsYtzAaCwXwkylTytkCPBmTOvjzaCww',       // Senha do banco de dados
   {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT
-   
+    host: 'tramway.proxy.rlwy.net',    // Endereço do host (exemplo: 'localhost' ou 'nomedohost.com')
+    port: 10300,               // Porta do banco de dados (o padrão para MySQL é 3306)
+    dialect: 'mysql',         // Tipo de banco de dados
+    dialectOptions: {
+      ssl: {
+        require: true,            // Exige SSL
+        rejectUnauthorized: false, // Não rejeitar certificados não verificados
+      },
+    },
   }
 );
+
+// Teste da conexão
+sequelize.authenticate()
+  .then(() => {
+    console.log('Conexão com o banco de dados estabelecida com sucesso!');
+  })
+  .catch((error) => {
+    console.error('Não foi possível conectar ao banco de dados:', error);
+  });
 
 module.exports = {
   Sequelize,
